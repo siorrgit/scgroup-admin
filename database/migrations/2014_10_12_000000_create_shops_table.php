a9e4c27b795445d9fb0112d30597bd57bfa9723d
itokuchen <takashi.ito@siorr.com>
2024-03-05 15:24:48 +0900

    feat: area_idカラムのmigrate順番を調整

--- database/migrations/2014_10_12_000000_create_shops_table.php
+++ database/migrations/2014_10_12_000000_create_shops_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('shops', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('area_id')->constrained()->nullable();
             $table->string('name');
             $table->string('shopID');
             $table->char('postcode', 7);
