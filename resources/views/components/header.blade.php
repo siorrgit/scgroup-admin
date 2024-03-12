fa24451c8cd4526d36877963459bd08f0682c7ef
kzkzsk <www.k.sakuma@gmail.com>
2024-03-12 14:49:44 +0900

    fix: Headerナビゲーションのログアウト機能実装

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -9,10 +9,20 @@
         <a class="mr-5 font-bold hover:text-blue-600">処方せん（完了）一覧</a>
         <a class="mr-5 font-bold hover:text-blue-600">ユーザー一覧</a>
       </nav>
-      <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">ログアウト
+      <form method="POST" action="{{ route('logout') }}">
+        @csrf
+
+        <a
+      class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 cursor-pointer"
+      :href="route('logout')"
+      onclick="event.preventDefault();
+                  this.closest('form').submit();"
+                  >ログアウト
         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
           <path d="M5 12h14M12 5l7 7-7 7"></path>
         </svg>
-      </button>
+      </a>
+    </form>
+
     </div>
   </header>
