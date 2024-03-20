12eb2ae9cc11dd0bb96f11b0bf5a79a00939c5a0
kzkzsk <www.k.sakuma@gmail.com>
2024-03-20 17:39:08 +0900

    fix: 店舗一覧の不要記述削除・css修正

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -1,30 +1,30 @@
 <x-app-layout>
     <section>
         <div class="container px-5 py-24 mx-auto">
-            <h1 class="text-center w-full text-2xl font-bold title-font text-gray-900">店舗一覧</h1>
+            <h1 class="text-center w-full text-2xl font-bold title-font">店舗一覧</h1>
           <div class="flex justify-end mt-10">
-            <x-primary-button-link href="/shop/add" class="text-m">新規店舗追加</x-primary-button-link>
+            <x-primary-button-link href="/shop/add" class="text-base">新規店舗追加</x-primary-button-link>
           </div>
           <div class="w-full mx-auto overflow-auto mt-5">
             <table class="table-auto w-full text-left whitespace-no-wrap">
               <thead>
                 <tr class="bg-gray-100">
-                  <th class="px-4 py-3 tracking-wider text-gray-900 text-m font-bold">無効</th>
-                  <th class="px-4 py-3 tracking-wider text-gray-900 text-m font-bold">エリア
+                  <th class="px-4 py-3 tracking-wider text-base font-bold">無効</th>
+                  <th class="px-4 py-3 tracking-wider text-base font-bold">エリア
                   </th>
-                  <th class="px-4 py-3 tracking-wider text-gray-900 text-m font-bold">ID</th>
-                  <th class="px-4 py-3 tracking-wider text-gray-900 text-m font-bold">店舗名</th>
-                  <th class="px-4 py-3 tracking-wider text-gray-900 text-m font-bold">詳細</th>
+                  <th class="px-4 py-3 tracking-wider text-base font-bold">ID</th>
+                  <th class="px-4 py-3 tracking-wider text-base font-bold">店舗名</th>
+                  <th class="px-4 py-3 tracking-wider text-base font-bold">詳細</th>
                 </tr>
               </thead>
               <tbody>
                 @foreach ($shops as $shop)
                 <tr>
-                    <td class="px-4 py-3 text-m"></td>
-                    <td class="px-4 py-3 text-m">{{ $shop->area->name }}</td>
-                    <td class="px-4 py-3 text-m">{{ $shop->id }}</td>
-                    <td class="px-4 py-3 text-m text-gray-900">{{ $shop->name }}</td>
-                    <td class="px-4 py-3"><a class="text-m text-blue-900" href={{'shop/' . $shop->id}}>詳細</a></td>
+                    <td class="px-4 py-3 text-base"></td>
+                    <td class="px-4 py-3 text-base">{{ $shop->area->name }}</td>
+                    <td class="px-4 py-3 text-base">{{ $shop->id }}</td>
+                    <td class="px-4 py-3 text-base">{{ $shop->name }}</td>
+                    <td class="px-4 py-3"><a class="text-base text-blue-900" href={{'shop/' . $shop->id}}>詳細</a></td>
                     </tr>
                     @endforeach
               </tbody>
