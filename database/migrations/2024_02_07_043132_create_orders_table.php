82e5e900d6ab13e715ab7d86ad9c0a2f64fe1267
itokuchen <takashi.ito@siorr.com>
2024-03-21 14:34:56 +0900

    feat: Orderの受取希望日をnullableに変更

--- database/migrations/2024_02_07_043132_create_orders_table.php
+++ database/migrations/2024_02_07_043132_create_orders_table.php
@@ -20,9 +20,10 @@ return new class extends Migration
             $table->string('guest_email')->nullable();
             $table->string('guest_phone')->nullable();
             $table->string('shop_id')->references('id')->on('shops');
-            $table->dateTime('receiving_at');
+            $table->dateTime('receiving_at')->nullable();
             $table->dateTime('received_at')->nullable();
             $table->string('status');
+            // 登録中 - register
             // 未完了 - incomplete
             // 未完了（来店依頼済み） - notified
             // 事前登録決済済み - apppayed
