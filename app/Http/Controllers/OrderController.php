0d93728424e0f435ff924e90ef1f7a96b76389d0
kzkzsk <www.k.sakuma@gmail.com>
2024-03-26 17:49:23 +0900

    feat: OrderController／処方せん一覧ページ（完了・未完了）で全ての店舗情報を渡す処理追加

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use App\Models\Shop;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -12,9 +13,11 @@ class OrderController extends Controller
         $orders = Order::query()
                        ->whereIn('status', ['incomplete', 'notified'])
                        ->get();
+        $shops = Shop::all();
 
         return view('order.index', [
             'orders' => $orders,
+            'shops' => $shops,
         ]);
     }
 
@@ -23,9 +26,11 @@ class OrderController extends Controller
         $orders = Order::query()
                        ->whereIn('status', ['apppayed', 'shoppayed', 'canceled'])
                        ->get();
+        $shops = Shop::all();
 
         return view('order.index', [
             'orders' => $orders,
+            'shops' => $shops,
         ]);
     }
 
